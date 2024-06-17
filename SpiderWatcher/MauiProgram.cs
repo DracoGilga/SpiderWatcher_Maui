dd02e0e63d87e7543772dc9be89afd4caea7f224
DracoGilga <cesar96707@gmail.com>
2024-06-17 02:39:59 -0600

    Edit: Mauiprogram, routes, and scripts

--- SpiderWatcher/MauiProgram.cs
+++ SpiderWatcher/MauiProgram.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.Extensions.Logging;
+using SpiderWatcher.Conexion;
+using SpiderWatcher.Services;
+using Microsoft.Maui.Hosting;
 
 namespace SpiderWatcher
 {
@@ -14,12 +17,28 @@ namespace SpiderWatcher
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 });
 
+            // Agregar Blazor WebView
             builder.Services.AddMauiBlazorWebView();
 
 #if DEBUG
-    		builder.Services.AddBlazorWebViewDeveloperTools();
-    		builder.Logging.AddDebug();
+            builder.Services.AddBlazorWebViewDeveloperTools();
+            builder.Logging.AddDebug();
 #endif
+
+            // Configuración específica del proyecto Blazor WebAssembly
+            var grpcBaseDeAddress = "https://grcpsw.azurewebsites.net";
+            var blobStorageConnectionString = "DefaultEndpointsProtocol=https;AccountName=spiderwatcherbs;AccountKey=fi8pH/Jt65gddZqKXB+SAXf0F2hGAtqksmRH7j6jBVqFhzqTKGu3tPT2TV0oD0edMo8HlbYqWSJ9+ASt/3odtQ==;EndpointSuffix=core.windows.net";
+            var imageContainerName = "imagecontent";
+            var videoContainerName = "videocontent";
+
+            builder.Services.AddScoped(sp => new AzureBlobService(blobStorageConnectionString, imageContainerName));
+            builder.Services.AddScoped(sp => new AzureVideoService(blobStorageConnectionString, videoContainerName));
+
+            var apiBaseDeAddress = "https://apigatewaysw.azurewebsites.net";
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(apiBaseDeAddress) });
+
+            builder.Services.AddScoped<AuthenticationService>();
+            builder.Services.AddScoped<NavigationService>();
 
             return builder.Build();
         }
